57fde9f8690046e04850b18f666b754a33f47be4
Kerinjo <konmarciniak@gmail.com>
2025-02-19 11:48:11 +0100

    Rest of the necessary Vector2 operations

--- basic-graphics-library/Vector2.cs
+++ basic-graphics-library/Vector2.cs
@@ -5,7 +5,7 @@ namespace basic_graphics_library;
 public class Vector2
 {
     private float[] _coordinates;
-    
+
     public Vector2(float x, float y)
     {
         _coordinates = [x, y];
@@ -15,7 +15,7 @@ public class Vector2
     {
         _coordinates = [0, 0];
     }
-    
+
     // Operator overloading
     // Indexing
     public float this[int i]
@@ -23,7 +23,7 @@ public class Vector2
         get => _coordinates[i];
         set => _coordinates[i] = value;
     }
-    
+
     // Addition
     public static Vector2 operator +(Vector2 a, Vector2 b)
     {
@@ -31,7 +31,7 @@ public class Vector2
             a[0] + b[0],
             a[1] + b[1]);
     }
-    
+
     // Subtraction
     public static Vector2 operator -(Vector2 a, Vector2 b)
     {
@@ -39,28 +39,29 @@ public class Vector2
             a[0] - b[0],
             a[1] - b[1]);
     }
+
+    // Dot product
+    public static float operator *(Vector2 a, Vector2 b)
+    {
+        return a[0] * a[1] + b[0] * b[1];
+    }
+
+    // Cross product
+    // In case of 2D Vector, return value is a scalar representing the z-component
+    public static float operator ^(Vector2 a, Vector2 b)
+    {
+        return a[0] * b[1] + b[0] * a[1];
+    }
     
-    // // Dot product
-    // public static float operator *(Vector2 a, Vector2 b)
-    // {
-    //     TODO
-    // }
-    //
-    // // Cross product
-    // public static Vector2 operator ^(Vector2 a, Vector2 b);
-    // {
-    //     TODO
-    // }
-    //
     // // Scalar multiplication
-    // public static Vector2 operator *(Vector2 a, float k)
-    // {
-    //     TODO
-    // }
-    //
-    // // Scalar division
-    // public static Vector2 operator /(Vector2 a, float k)
-    // {
-    //     TODO
-    // }
+    public static Vector2 operator *(Vector2 a, float k)
+    {
+        return new Vector2(k * a[0], k * a[1]);
+    }
+    
+    // Scalar division
+    public static Vector2 operator /(Vector2 a, float k)
+    {
+        return new Vector2(a[0] / k, a[1] / k);
+    }
 }
---
+++ basic-graphics-library/notes.md
@@ -0,0 +1,4 @@
+## Vector2
+- Could add unary minus if needed.
+- Equality. Normally for vectors that would be comparing direction and length,
+but for vector2 it's probably better to just compare coordinates since we can't change origin yet.
